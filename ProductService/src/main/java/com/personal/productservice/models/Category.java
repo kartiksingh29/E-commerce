903ff7b254e7db78a97ccff0b51e005845ece9ef
kartik singh <kartiksingh@kartiks-MacBook-Air.local>
2025-08-07 01:35:00 +0530

    updated category model  back to normal

--- ProductService/src/main/java/com/personal/productservice/models/Category.java
+++ ProductService/src/main/java/com/personal/productservice/models/Category.java
@@ -4,29 +4,22 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
 
 @Getter
 @Setter
 @Entity
 public class Category extends BaseModel {
-    // this mappedBy tells spring to not create any new mapping for category-product relation.
-
-    // cascade-remove means if category removed, then remove all corresponding products also.
-
-    // fetch type as Lazy ensures this field is not fetched right away when fetching Category
-    // from database to improve query performance.
-    @OneToMany(mappedBy="category", fetch = FetchType.LAZY, cascade = {CascadeType.REMOVE})
-    private List<Product> productList;
-
     private String name ;
 }
 
 
+// Update : removed List<Product> field because it needs careful handling
+//          and also because it was just for demo purpose/ understanding purpose.
+
 
 // Here, we have List<Product> productList field in Category Class.
 // This bi-direction association for Product-Category is legal in Java.
-// But it needs to be handled carefully so that it doesn't go in infinite loop.
+// But it needs to be handled carefully so that it doesn't go in infinite loop ( circular-dependency ).
 
 // Example if we define custom constructor in both models, then Product object creation
 // would require a Category object and similarly Category object creation would require
