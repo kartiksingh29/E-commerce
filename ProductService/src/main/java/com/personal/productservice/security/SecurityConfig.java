b4b0e641caafa2f283059c357c5ab91f202de955
kartik singh <kartiksingh@kartiks-MacBook-Air.local>
2025-08-14 02:36:45 +0530

    Added role based authentication for /products/{id} API

--- ProductService/src/main/java/com/personal/productservice/security/SecurityConfig.java
+++ ProductService/src/main/java/com/personal/productservice/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.personal.productservice.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
 
@@ -13,11 +15,23 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().authenticated()
+                        .requestMatchers("/products/{id}").hasAuthority("SCOPE_ADMIN")
+                        .anyRequest().permitAll()
                 )
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
 }
 
-// this SecurityFilterChain bean ensures all http requests are authenticated
+// this SecurityFilterChain bean ensures all http requests are permitted
+// except http://localhost:8080/products/{id} which is authenticated
